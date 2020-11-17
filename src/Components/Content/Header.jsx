41da8628eb85909c3e0de24dcaca1fc2de3ea226
DariusPirvulescu <organicdarius@gmail.com>
2020-11-17 02:18:11 +0200

    Import avatar and cover photo for mobile. Made cover photo responsive. Add send-message btn

--- src/Components/Content/Header.jsx
+++ src/Components/Content/Header.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import { BiLike } from "react-icons/bi";
+import Button from "@material-ui/core/Button";
+import { AiFillLike } from "react-icons/ai";
 import { MdRssFeed } from "react-icons/md";
 import { IoMdShareAlt } from "react-icons/io";
 import { BsBookmarkFill } from "react-icons/bs";
 
 import "./Header.scss";
 
+const ResponsiveImage = ({ src, width, height }) => {
+  return (
+    <div
+      style={{
+        width,
+      }}
+      className="responsive-img"
+    >
+      <div
+        style={{
+          paddingBottom: (height / width) * 100 + "%",
+        }}
+      />
+      <img src={src} className="responsive-img__img" alt="Flowmatters cover"/>
+    </div>
+  );
+};
+
 const Header = () => {
+  // const smallCover = "img/cover-sm.png";
+  // const webCover = "img/cover.jpg";
+
   return (
     <div className="Header">
-      <div className="cover">
-        <img src="img/cover.jpg" alt="Flowmatters cover" />
-      </div>
+      {/* <div className="cover"> */}
+      {/* <img src="img/cover.jpg" srcSet={`${smallCover} 400w,${"img/avatar.jpg"} 600w` } alt="Flowmatters cover" /> */}
+      <ResponsiveImage src="img/cover-sm.png" width={640} height={281} />
+      {/* </div> */}
       <div className="info">
         <div className="avatar">
-          <img src="img/avatar.jpg" alt="Flowmatters avatar" />
+          <img src="img/avatar-sm.png" alt="Flowmatters avatar" />
         </div>
         <div className="title">
           <p>Flowmatters</p>
@@ -27,21 +50,31 @@ const Header = () => {
         </div>
       </div>
       <div className="actions">
-        <div>
-          <BiLike className="action-icon" />
-          <p>Like</p>
-        </div>
-        <div>
-          <MdRssFeed className="action-icon" />
-          <p>Follow</p>
-        </div>
-        <div>
-          <IoMdShareAlt className="action-icon" />
-          <p>Share</p>
-        </div>
-        <div>
-          <BsBookmarkFill className="action-icon" />
-          <p>Save</p>
+        <Button
+          variant="contained"
+          color="primary"
+          href="#"
+          className="btn-send-msg"
+        >
+          Send Message
+        </Button>
+        <div className="main-actions">
+          <div>
+            <AiFillLike className="action-icon" />
+            <p>Like</p>
+          </div>
+          <div>
+            <MdRssFeed className="action-icon" />
+            <p>Follow</p>
+          </div>
+          <div>
+            <IoMdShareAlt className="action-icon" />
+            <p>Share</p>
+          </div>
+          <div>
+            <BsBookmarkFill className="action-icon" />
+            <p>Save</p>
+          </div>
         </div>
       </div>
     </div>
