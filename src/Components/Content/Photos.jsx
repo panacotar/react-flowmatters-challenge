616e98e4c79552a61bf777cc5e6bfbc15726eee2
DariusPirvulescu <organicdarius@gmail.com>
2020-11-17 20:04:59 +0200

    Refactor code in Photos.jsx

--- src/Components/Content/Photos.jsx
+++ src/Components/Content/Photos.jsx
@@ -1,31 +1,17 @@
 import React from "react";
-import Imgix from "react-imgix";
 
 import "./Photos.scss";
 import data from "../../data/photos-src.json";
 
 const Photos = () => {
-  console.log(data);
-
   return (
     <div className="photos">
       <div className="photos-header">Photos</div>
       <div className="photos-gallery">
         {data.map((src) => {
-          {
-            /* return <img src={src.thumbnail} key={src.id} className="page-image" alt="thumbnail"  /> */
-          }
-
           return (
-            <div className="page-image">
-              <Imgix
-                src={src.thumbnail}
-                key={src.id}
-                
-                imgixParams={{
-                  fit: "crop",
-                }}
-              />
+            <div className="page-image" key={src.id}>
+              <img src={src.thumbnail} alt="thumbnail" />
             </div>
           );
         })}
