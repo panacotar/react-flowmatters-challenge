733622bd26b67fa75adc481e06db66d14084489d
DariusPirvulescu <organicdarius@gmail.com>
2020-11-18 15:39:27 +0200

    Add Material UI tabs to Links component

--- src/Components/Content/Links.jsx
+++ src/Components/Content/Links.jsx
@@ -1,17 +1,42 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import React, { useState } from "react";
+import { Tabs, Tab } from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./Links.scss";
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    height: 20,
+  },
+});
+
 const Links = () => {
+  const classes = useStyles();
+  const [value, setIndex] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setIndex(newValue);
+  };
 
   return (
     <div className="links">
-      <Button>Home</Button>
-      <Button>Posts</Button>
-      <Button>Photos</Button>
-      <Button>Videos</Button>
-      <Button>Info</Button>
+      <Tabs
+        className={classes.root}
+        style={{ height: "20px" }}
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        aria-label="icon label tabs example"
+        indicatorColor="primary"
+        textColor="primary"
+      >
+        <Tab label={<div>Home </div>} />
+        <Tab label={<div>Posts </div>} />
+        <Tab label={<div>Photos </div>} />
+        <Tab label={<div>Videos </div>} />
+        <Tab label={<div>Info </div>} />
+      </Tabs>
     </div>
   );
 };
