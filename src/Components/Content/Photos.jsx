2e25a556da5aa462ecaa9ccd3361f6965a379a66
DariusPirvulescu <organicdarius@gmail.com>
2020-11-19 21:39:25 +0200

    Create PhotoModal Component, import in Photos Component

--- src/Components/Content/Photos.jsx
+++ src/Components/Content/Photos.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Photos.scss";
 import data from "../../data/photos-src.json";
+import PhotoModal from "../Modal/PhotoModal";
 
 const Photos = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedImg, setSelectedImg] = useState("");
+
+  const handleOpen = (evt) => {
+    const searchedId = parseInt(evt.target.id);
+    const foundImg = data.find((img) => searchedId === img.id);
+
+    setSelectedImg(foundImg.full);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setTimeout(() => {
+      setSelectedImg("");
+    }, 500);
+  };
+
   return (
     <div className="photos">
       <div className="photos-header">Photos</div>
       <div className="photos-gallery">
         {data.map((src) => {
           return (
-            <div className="photo-flex-item" key={src.id}>
-              <img src={src.thumbnail} alt="thumbnail" />
+            <div className="photo-flex-item" key={src.id} onClick={handleOpen}>
+              <img id={src.id} src={src.thumbnail} alt="thumbnail" />
             </div>
           );
         })}
       </div>
+      <PhotoModal open={open} src={selectedImg} onClose={handleClose} />
     </div>
   );
 };
---
+++ src/Components/Modal/PhotoModal.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Grow from "@material-ui/core/Grow";
+
+import "./PhotoModal.scss";
+
+const useStyles = makeStyles(() => ({
+  paper: {
+    margin: "auto",
+    marginTop: "20vh",
+    width: 400,
+    height: 400,
+    backgroundColor: "none",
+    "&:focus": {
+      outline: "none",
+    },
+  },
+}));
+
+const PhotoModal = (props) => {
+  const classes = useStyles();
+
+  const body = (
+    <div className={classes.paper}>
+      <img
+        src={props.src}
+        alt="Selected in Modal"
+        style={{ width: "100%", height: "100%" }}
+      />
+    </div>
+  );
+
+  return (
+    <div className="Modal">
+      <Grow in={props.open}>
+        <Modal open={props.open} onClose={props.onClose} keepMounted>
+          {body}
+        </Modal>
+      </Grow>
+    </div>
+  );
+};
+
+export default PhotoModal;
