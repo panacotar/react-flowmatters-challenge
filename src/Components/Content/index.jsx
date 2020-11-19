d6d66aea67afe257d0d950ab2d553adf0798f077
DariusPirvulescu <organicdarius@gmail.com>
2020-11-19 17:51:44 +0200

    Extend Content component for Desktop display

--- src/Components/Content/index.jsx
+++ src/Components/Content/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 import "./Content.scss";
 import Header from "./Header";
@@ -6,15 +7,42 @@ import Photos from "./Photos";
 import Links from "./Links";
 import StatusPost from "./Post/StatusPost";
 import Post from "./Post/Post";
+import ContentSidebar from "./ContentSidebar";
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+  return isDesktop ? children : null;
+};
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
 
 const Content = () => {
   return (
-    <div className="content">
-      <Header />
-      <Links />
-      <Photos />
-      <StatusPost />
-      <Post />
+    <div>
+      <Mobile>
+        <div className="content">
+          <Header />
+          <Links />
+          <Photos />
+          <StatusPost />
+          <Post />
+        </div>
+      </Mobile>
+      <Desktop>
+        <div className="content">
+          <Header />
+          <div className="content-flex">
+            <div className="posts">
+              <StatusPost />
+              <Post />
+            </div>
+            <ContentSidebar />
+          </div>
+        </div>
+      </Desktop>
     </div>
   );
 };
