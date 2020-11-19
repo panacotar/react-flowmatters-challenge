0260599746e775cc58b3c1827248fa7b079f0749
DariusPirvulescu <organicdarius@gmail.com>
2020-11-19 13:36:39 +0200

    Add custom global theme for material UI components

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,3 +1,8 @@
+import React from "react";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import "./App.scss";
 import NavBar from "./Navbar";
 import SideBar from "./Sidebar";
@@ -5,15 +10,32 @@ import Content from "./Content";
 import BottomNav from "./Bottom-nav/BottomNav";
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: { main: "#3B5998" },
+      secondary: { main: "#f6f7f9" },
+    },
+    typography: {
+      button: {
+        textTransform: "none",
+        // fontFamily: ["Helvetica Neue", "Arial"],
+        fontWeight: 600,
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <NavBar />
-      <div className="main">
-        <SideBar />
-        <Content />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <NavBar />
+        <div className="main">
+          <SideBar />
+          <Content />
+        </div>
+        <BottomNav />
       </div>
-      <BottomNav />
-    </div>
+    </ThemeProvider>
   );
 }
 
