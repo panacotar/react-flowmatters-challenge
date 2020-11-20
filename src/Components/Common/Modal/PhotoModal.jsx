9b250bac0d4b280420d61cd0e8fab7c1b1d7241d
DariusPirvulescu <organicdarius@gmail.com>
2020-11-20 18:55:02 +0200

    Fix PhotoModal in Mobile and Landscape views

--- src/Components/Common/Modal/PhotoModal.jsx
+++ src/Components/Common/Modal/PhotoModal.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useMediaQuery } from "react-responsive";
 import Modal from "@material-ui/core/Modal";
 import Grow from "@material-ui/core/Grow";
 
-const useStyles = makeStyles(() => ({
-  paper: {
-    margin: "auto",
-    marginTop: "20vh",
-    width: 400,
-    height: 400,
-    backgroundColor: "none",
-    "&:focus": {
-      outline: "none",
-    },
-  },
-}));
-
 const PhotoModal = (props) => {
+  const isLandscape = useMediaQuery({ minWidth: 507, maxWidth: 768 });
+  const isDesktop = useMediaQuery({ minWidth: 768 });
+
+  const width = isDesktop ? "400px" : "300px",
+    height = width;
+  const marginTop = isLandscape ? "0" : "10vh";
+
+  const useStyles = makeStyles(() => ({
+    paper: {
+      margin: "auto",
+      marginTop: marginTop,
+      width: width,
+      height: height,
+      backgroundColor: "none",
+      "&:focus": {
+        outline: "none",
+      },
+    },
+  }));
+
   const classes = useStyles();
 
   const body = (
